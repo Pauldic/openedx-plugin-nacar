111d617a520872a0f9ba14c791ed4f2a458968bb
Paul Okeke <pauldiconline@gmail.com>
2025-09-03 19:52:09 +0100

    Fixing the template indiction

--- openedx_plugin_cms/settings/common.py
+++ openedx_plugin_cms/settings/common.py
@@ -29,20 +29,20 @@ def plugin_settings(settings):
         settings.MAKO_TEMPLATE_DIRS_BASE = list(settings.MAKO_TEMPLATE_DIRS_BASE)  # ensure mutable
         settings.MAKO_TEMPLATE_DIRS_BASE.insert(0, TEMPLATES_DIR)  # prepend to have priority
 
-    # 2. Django templates (for render_to_string / email templates)
-    if hasattr(settings, "TEMPLATES") and settings.TEMPLATES:
-        # Get current DIRS and normalize them
-        current_dirs = settings.TEMPLATES[0].get("DIRS", [])
-        dirs = []
-        for d in current_dirs:
-            if isinstance(d, (list, tuple)):
-                dirs.extend(str(x) for x in d if x)
-            elif d:
-                dirs.append(str(d))
+    # # 2. Django templates (for render_to_string / email templates)
+    # if hasattr(settings, "TEMPLATES") and settings.TEMPLATES:
+    #     # Get current DIRS and normalize them
+    #     current_dirs = settings.TEMPLATES[0].get("DIRS", [])
+    #     dirs = []
+    #     for d in current_dirs:
+    #         if isinstance(d, (list, tuple)):
+    #             dirs.extend(str(x) for x in d if x)
+    #         elif d:
+    #             dirs.append(str(d))
 
-        # Always prepend our plugin's templates dir
-        dirs.insert(0, str(TEMPLATES_DIR))
-        settings.TEMPLATES[0]["DIRS"] = dirs
+    #     # Always prepend our plugin's templates dir
+    #     dirs.insert(0, str(TEMPLATES_DIR))
+    #     settings.TEMPLATES[0]["DIRS"] = dirs
 
     # 3. Optional: static files directory if your plugin has static assets
     if hasattr(settings, "STATICFILES_DIRS"):
