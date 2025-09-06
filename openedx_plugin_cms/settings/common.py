f154a8166831101ad4bee4f70b6f791df0d008bc
Paul Okeke <pauldiconline@gmail.com>
2025-09-06 20:52:52 +0100

    Fixing common template settings

--- openedx_plugin_cms/settings/common.py
+++ openedx_plugin_cms/settings/common.py
@@ -6,6 +6,7 @@ Oct-2021
 Common Pluggable Django App settings
 """
 from path import Path as path
+from openedx.core.lib.derived import Derived
 
 APP_ROOT = (path(__file__).abspath().dirname().dirname())  # /blah/blah/blah/.../nacar-digital-learning-openedx/openedx_plugin
 TEMPLATES_DIR = APP_ROOT / "templates"
@@ -26,7 +27,7 @@ def plugin_settings(settings):
     # settings.MAKO_TEMPLATE_DIRS_BASE.extend([TEMPLATES_DIR])    
     # 1. Mako templates (keep this if you have any Mako-based templates)
     if hasattr(settings, "MAKO_TEMPLATE_DIRS_BASE"):
-        print(f">>>>>>>>>>>>>>>  settings.MAKO_TEMPLATE_DIRS_BASE ", settings.MAKO_TEMPLATE_DIRS_BASE)
+        print(f" MAKO: >>>>>>>>>>>>>>>  {type(settings.MAKO_TEMPLATE_DIRS_BASE)} settings.MAKO_TEMPLATE_DIRS_BASE ", settings.MAKO_TEMPLATE_DIRS_BASE)
         settings.MAKO_TEMPLATE_DIRS_BASE = list(settings.MAKO_TEMPLATE_DIRS_BASE)  # ensure mutable
         # print(f">>>>>>>>>>>>>>>>>>>>>>>>>> Adding AA {TEMPLATES_DIR} to {settings.MAKO_TEMPLATE_DIRS_BASE}")
         settings.MAKO_TEMPLATE_DIRS_BASE.insert(0, TEMPLATES_DIR)  # prepend to have priority
@@ -34,10 +35,16 @@ def plugin_settings(settings):
 
     # 2. Django templates (for render_to_string / email templates)t 
     if hasattr(settings, "TEMPLATES") and settings.TEMPLATES:
-        print(f">>>>>>>>>>>>>>>  settings.TEMPLATES ", settings.TEMPLATES)
-        # print(f">>>>>>>>>>>>>>>>>>>>>>>>>> Adding BB {TEMPLATES_DIR} to {settings.TEMPLATES[0]['DIRS']}")
-        settings.TEMPLATES[0]["DIRS"].insert(0, str(TEMPLATES_DIR))
-        # settings.TEMPLATES[0]["DIRS"].append(str(TEMPLATES_DIR))
-        # settings.TEMPLATES[0]["DIRS"] = [str(TEMPLATES_DIR)] + list(settings.TEMPLATES[0]["DIRS"])
+        print(f"TEMPLATES 1: >>>>>>>>>>>>>>> {type(settings.TEMPLATES[0]["DIRS"])}  settings.TEMPLATES ", settings.TEMPLATES)  
+        if isinstance(settings.TEMPLATES[0]["DIRS"], Derived):      
+            dirs = list(settings.TEMPLATES[0]["DIRS"])
+            print(f"TEMPLATES 2: >>>>>>>>>>>>>>>  settings.TEMPLATES (List): ", dirs)
+            dirs.insert(0, str(TEMPLATES_DIR))
+            settings.TEMPLATES[0]["DIRS"] = dirs
+        else:            
+            # print(f">>>>>>>>>>>>>>>>>>>>>>>>>> Adding BB {TEMPLATES_DIR} to {settings.TEMPLATES[0]['DIRS']}")
+            settings.TEMPLATES[0]["DIRS"].insert(0, str(TEMPLATES_DIR))
+            # settings.TEMPLATES[0]["DIRS"].append(str(TEMPLATES_DIR))
+            # settings.TEMPLATES[0]["DIRS"] = [str(TEMPLATES_DIR)] + list(settings.TEMPLATES[0]["DIRS"])
 
 
