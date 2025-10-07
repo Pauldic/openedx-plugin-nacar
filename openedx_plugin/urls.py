dadba6e6f90f64a023ac44f2991a45e3e628b140
Paul Okeke <pauldiconline@gmail.com>
2025-10-07 14:42:28 +0100

    added enrollment_list view

--- openedx_plugin/urls.py
+++ openedx_plugin/urls.py
@@ -8,8 +8,8 @@ https://lms.yourdomain.edu/openedx_plugin/dashboard
 https://lms.yourdomain.edu/openedx_plugin/dashboard?language=es-419
 """
 # Django
+from django.urls import path
 from django.urls import re_path
-
 # this repo
 from openedx_plugin.dashboard.views import student_dashboard
 from openedx_plugin.locale.views import marketing_redirector
@@ -18,12 +18,12 @@ from .waffle import waffle_switches, AUTOMATED_ENROLLMENT, MARKETING_REDIRECTOR
 
 app_name = "openedx_plugin"
 
-urlpatterns = []
+urlpatterns = [    
+    path("courses/<str:course_id>/instructor/enrollment_list/", views.enrollment_list_view, name="nacar_enrollment_list"),
+]
 
 if waffle_switches[AUTOMATED_ENROLLMENT]:
-    urlpatterns += [
-        re_path(r"^dashboard/?$", student_dashboard, name="nacar_dashboard"),
-    ]
+    urlpatterns += [re_path(r"^dashboard/?$", student_dashboard, name="nacar_dashboard"),]
 
 if waffle_switches[MARKETING_REDIRECTOR]:
     urlpatterns += [
---
+++ openedx_plugin/views.py
@@ -0,0 +1,49 @@
+# openedx-plugin-nacac/openedx_plugin/views.py
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.utils.decorators import method_decorator
+from django.views.decorators.clickjacking import xframe_options_exempt
+from opaque_keys.edx.keys import CourseKey
+from common.djangoapps.student.models import CourseEnrollment
+from lms.djangoapps.courseware.courses import get_course_by_id
+from common.djangoapps.student.roles import CourseInstructorRole, CourseStaffRole
+
+
+@login_required
+@xframe_options_exempt
+def enrollment_list_view(request, course_id):
+    course_key = CourseKey.from_string(course_id)
+
+    # Check permissions: only instructors/staff
+    if not (
+        CourseInstructorRole(course_key).has_user(request.user) or
+        CourseStaffRole(course_key).has_user(request.user)
+    ):
+        raise PermissionDenied
+
+    course = get_course_by_id(course_key)
+
+    enrollments = CourseEnrollment.objects.filter(
+        course_id=course_key,
+        is_active=True
+    ).select_related('user').order_by('user__username')
+
+    students = []
+    for enrollment in enrollments:
+        user = enrollment.user
+        students.append({
+            'username': user.username,
+            'email': user.email,
+            'full_name': f"{user.first_name} {user.last_name}".strip() or user.username,
+            'enrollment_date': enrollment.created,
+            'mode': enrollment.mode,
+        })
+
+    context = {
+        'course': course,
+        'students': students,
+        'course_id': str(course_key),
+    }
+
+    return render(request, 'openedx_plugin/enrollment_list.html', context)
